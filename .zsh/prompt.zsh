6ea8c1f7c4caf1d1c0c2e520a876f698513cc9a5
silverwind <me@silverwind.io>
2015-12-14 15:43:18 +0100

    add exit code to right prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -5,4 +5,4 @@ zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:git*' formats "%{$fg[grey]%}%s %{$fg[blue]%}%b%{$reset_color%}%m%u%c%{$reset_color%}"
 precmd () { vcs_info }
 PS1='%{$fg[red]%}%n%{$fg[yellow]%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$fg[blue]%}$ %{$reset_color%}'
-RPS1='${vcs_info_msg_0_}'
+RPS1='%(?..%{$fg[red]%}%?%{$reset_color%} )${vcs_info_msg_0_}'
