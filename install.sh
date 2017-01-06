d26a9bce7ea0fff2de27899a12e5eb606c8d52b9
silverwind <me@silverwind.io>
2017-01-06 20:16:36 +0100

    expand install script to cover more files

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+declare -a files=(
+  .dircolors
+  .editorconfig
+  .eslintrc
+  .gitconfig
+  .hgrc
+  .htoprc
+  .hushlogin
+  .inputrc
+  .ls++.conf
+  .curlrc
+  .wgetrc
+)
+
 set -x
 
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-rm -rf ~/.gitconfig
-ln -s "$(pwd)/.gitconfig" ~
+for file in "${files[@]}"; do
+  rm -rf "$HOME/$file"
+  ln -s "$(pwd)/$file" "$HOME"
+done
 
 source "$DIR/install-vim.sh"
