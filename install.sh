1c4a783a8a733da318e9d5ac0519b575650cd212
silverwind <me@silverwind.io>
2017-03-06 17:39:37 +0100

    merge install scripts into one

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+###############################################################################
+# general dotfiles
+###############################################################################
+
 declare -a files=(
   .dircolors
   .editorconfig
@@ -22,3 +26,28 @@ for file in "${files[@]}"; do
 done
 
 source "$DIR/install-vim.sh"
+
+
+###############################################################################
+# vim / nvim
+###############################################################################
+
+# clean up
+rm -rf ~/.vim
+rm -rf ~/.vimrc
+rm -rf ~/.config/nvim
+
+# vim
+ln -s "$(pwd)/.vimrc" ~
+ln -s "$(pwd)/.vim" ~
+
+# nvim
+mkdir -p ~/.config
+ln -s "$(pwd)/.vim" ~/.config/nvim
+ln -s "$(pwd)/.vimrc" ~/.config/nvim/init.vim
+
+# get plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# install plugins
+vim -c ":PlugInstall"
