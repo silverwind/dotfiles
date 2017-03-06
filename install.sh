814b23483e4443db3d9c767c71c8fd845601d985
silverwind <me@silverwind.io>
2017-03-06 17:44:56 +0100

    tweak/optimize install script

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 # general dotfiles
 ###############################################################################
 
+set -x
+
+cwd="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
 declare -a files=(
   .dircolors
   .editorconfig
@@ -18,36 +22,30 @@ declare -a files=(
   .wgetrc
 )
 
-DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-
 for file in "${files[@]}"; do
   rm -rf "$HOME/$file"
-  ln -s "$DIR/$file" "$HOME"
+  ln -s "$cwd/$file" "$HOME"
 done
-
-source "$DIR/install-vim.sh"
-
 
 ###############################################################################
 # vim / nvim
 ###############################################################################
 
 # clean up
-rm -rf ~/.vim
-rm -rf ~/.vimrc
-rm -rf ~/.config/nvim
+rm -rf "$HOME/.vim"
+rm -rf "$HOME/.vimrc"
+rm -rf "$HOME/.config/nvim"
 
 # vim
-ln -s "$(pwd)/.vimrc" ~
-ln -s "$(pwd)/.vim" ~
+ln -s "$cwd/.vimrc" "$HOME"
+ln -s "$cwd/.vim" "$HOME"
 
 # nvim
-mkdir -p ~/.config
-ln -s "$(pwd)/.vim" ~/.config/nvim
-ln -s "$(pwd)/.vimrc" ~/.config/nvim/init.vim
+mkdir -p "$HOME/.config"
+ln -s "$cwd/.vim" "$HOME/.config/nvim"
 
 # get plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # install plugins
 vim -c ":PlugInstall"
